00277a1385e434ee0969482984921ac226b784e3
김기훈 <ki_hoon.kim@samsung.com>
2022-01-31 09:36:51 +0900

    first version
    Signed-off-by: 김기훈 <ki_hoon.kim@samsung.com>

---
+++ backend.py
@@ -0,0 +1,237 @@
+from typing import List, Set, Dict, Tuple
+from enum import Enum
+import random
+import time
+from front import Front
+import copy
+
+
+class Pos:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+
+class Color(Enum):
+    BLUE = 1
+    RED = 2
+    ORANGE = 3
+    GREEN = 4
+    PURPLE = 5
+    YELLO = 6
+
+
+class Move(Enum):
+    TURN = 0
+    LEFT = 1
+    RIGHT = 2
+    DROP = 3
+    DOWN = 4
+
+
+class MoveFeedback(Enum):
+    SUCCESS = 0
+    IMPOSSIBLE = 1
+    STACKED = 2
+
+
+class BlockType(Enum):
+    BAR = 0
+    RECT = 1
+    Z = 2
+    Z_R = 3
+    MOUNTAIN = 4
+    SEVEN = 5
+    SEVEN_R = 6
+
+
+class Block:
+    def __init__(self) -> None:
+        self.center = Pos(4, 2)
+        self.turn_count = random.randint(0, 3)
+        self.color = random.choice(list(Color)).value
+
+    @property
+    def cubes(self) -> List[List[int]]:
+        pass
+
+    def IsPositionable(self, map) -> bool:
+        if not all(0 <= cube[0] < 10 and 0 <= cube[1] < 25 for cube in self.cubes):
+            return False
+        if not all(map[cube[1]][cube[0]] == 0 for cube in self.cubes):
+            return False
+        return True
+
+    def move(self, direction: Move, map):
+        if direction.value == Move.TURN.value:
+            self.turn_count += 1
+            if not self.IsPositionable(map=map):
+                self.turn_count -= 1
+                return MoveFeedback.IMPOSSIBLE
+
+        elif direction.value == Move.LEFT.value:
+            self.center.x -= 1
+            if not self.IsPositionable(map=map):
+                self.center.x += 1
+                return MoveFeedback.IMPOSSIBLE
+
+        elif direction.value == Move.RIGHT.value:
+            self.center.x += 1
+            if not self.IsPositionable(map=map):
+                self.center.x -= 1
+                return MoveFeedback.IMPOSSIBLE
+
+        elif direction.value == Move.DOWN.value:
+            self.center.y += 1
+            if not self.IsPositionable(map=map):
+                self.center.y -= 1
+                return MoveFeedback.STACKED
+
+        elif direction.value == Move.DROP.value:
+            self.center.y += 1
+            if not self.IsPositionable(map=map):
+                self.center.y -= 1
+                return MoveFeedback.STACKED
+            else:
+                self.move(direction=Move.DROP, map=map)
+                return MoveFeedback.STACKED
+
+        return MoveFeedback.SUCCESS
+
+
+class BlockBar(Block):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def cubes(self) -> List[List[int]]:
+        if self.turn_count % 2 == 0:
+            return [[self.center.x, self.center.y - 1, self.color], [self.center.x, self.center.y, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x, self.center.y + 2, self.color]]
+        else:
+            return [[self.center.x - 1, self.center.y, self.color], [self.center.x, self.center.y, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x + 2, self.center.y, self.color]]
+
+
+class BlockRect(Block):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def cubes(self) -> List[List[int]]:
+        return [[self.center.x, self.center.y, self.color], [self.center.x + 1, self.center.y + 1, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x, self.center.y + 1, self.color]]
+
+class BlockZ(Block):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def cubes(self) -> List[List[int]]:
+        if self.turn_count % 2 == 0:
+            return [[self.center.x, self.center.y, self.color], [self.center.x - 1, self.center.y, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x + 1, self.center.y + 1, self.color]]
+        elif self.turn_count % 2 == 1:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x + 1, self.center.y - 1, self.color]]
+class BlockZR(Block):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def cubes(self) -> List[List[int]]:
+        if self.turn_count % 2 == 0:
+            return [[self.center.x, self.center.y, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x - 1, self.center.y + 1, self.color]]
+        elif self.turn_count % 2 == 1:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y - 1, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x + 1, self.center.y + 1, self.color]]
+
+class BlockMountain(Block):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def cubes(self) -> List[List[int]]:
+        if self.turn_count % 4 == 0:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y - 1, self.color], [self.center.x - 1, self.center.y, self.color], [self.center.x + 1, self.center.y, self.color]]
+        elif self.turn_count % 4 == 1:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y - 1, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x - 1, self.center.y, self.color]]
+        elif self.turn_count % 4 == 2:
+            return [[self.center.x, self.center.y, self.color], [self.center.x - 1, self.center.y, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x, self.center.y + 1, self.color]]
+        elif self.turn_count % 4 == 3:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y - 1, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x + 1, self.center.y, self.color]]
+
+class BlockSeven(Block):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def cubes(self) -> List[List[int]]:
+        if self.turn_count % 4 == 0:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y - 1, self.color], [self.center.x - 1, self.center.y - 1, self.color], [self.center.x, self.center.y + 1, self.color]]
+        elif self.turn_count % 4 == 1:
+            return [[self.center.x, self.center.y, self.color], [self.center.x - 1, self.center.y, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x - 1, self.center.y + 1, self.color]]
+        elif self.turn_count % 4 == 2:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x + 1, self.center.y + 1, self.color], [self.center.x, self.center.y - 1, self.color]]
+        elif self.turn_count % 4 == 3:
+            return [[self.center.x, self.center.y, self.color], [self.center.x - 1, self.center.y, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x + 1, self.center.y - 1, self.color]]
+
+class BlockSevenR(Block):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def cubes(self) -> List[List[int]]:
+        if self.turn_count % 4 == 0:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y - 1, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x + 1, self.center.y - 1, self.color]]
+        elif self.turn_count % 4 == 1:
+            return [[self.center.x, self.center.y, self.color], [self.center.x - 1, self.center.y, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x - 1, self.center.y - 1, self.color]]
+        elif self.turn_count % 4 == 2:
+            return [[self.center.x, self.center.y, self.color], [self.center.x, self.center.y + 1, self.color], [self.center.x, self.center.y - 1, self.color], [self.center.x - 1, self.center.y + 1, self.color]]
+        elif self.turn_count % 4 == 3:
+            return [[self.center.x, self.center.y, self.color], [self.center.x - 1, self.center.y, self.color], [self.center.x + 1, self.center.y, self.color], [self.center.x + 1, self.center.y + 1, self.color]]
+
+
+block_list = [BlockBar, BlockRect, BlockZ, BlockZR, BlockMountain, BlockSeven, BlockSevenR]
+
+
+class Tetris:
+    def __init__(self) -> None:
+        self.map: List[List[int]] = []
+        self.total_score: int = 0
+        self.blockQueue: List[Block] = []
+        self.currentBlock: Block = random.choice(block_list)()
+        self.front = Front()
+
+        for i in range(25):
+            self.map.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def tick(self, command) -> int:
+        feedback = None
+        if command is not -1:
+            feedback = self.currentBlock.move(command, self.map)
+        score = 0
+        _map = copy.deepcopy(self.map)
+        for cube in self.currentBlock.cubes:
+            _map[cube[1]][cube[0]] = cube[2]
+        self.front.show(map=_map)
+
+        if feedback is not None and feedback.value == MoveFeedback.STACKED.value:
+            for cube in self.currentBlock.cubes:
+                self.map[cube[1]][cube[0]] = cube[2]
+                self.currentBlock = random.choice(block_list)()
+
+            for i in range(25):
+                while all(cube > 0 for cube in self.map[i]):
+                    self.map.pop(i)
+                    self.map.insert(0, [0] * 10)
+        return score
+
+    def run(self):
+        while True:
+            command = self.front.get_command()
+            self.tick(command=command)
+            time.sleep(0.1)
+
+
+t = Tetris()
+t.run()
+
+
+"""
+
+"""
---
+++ front.py
@@ -0,0 +1,123 @@
+from curses import KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_UP
+import pygame
+from enum import Enum
+
+pygame.init()
+
+
+class Color(Enum):
+    BLUE = 1
+    RED = 2
+    ORANGE = 3
+    GREEN = 4
+    PURPLE = 5
+    YELLO = 6
+
+
+BLACK = (0,  0,  0)
+WHITE = (255, 255, 255)
+BLUE = (0,  0, 255)
+GREEN = (0, 255,  0)
+RED = (255,  0,  0)
+ORANGE = (255,  0,  0)
+PURPLE = (255,  0,  0)
+YELLO = (255,  0,  0)
+
+colors = {0: WHITE, 1: BLUE, 2: RED, 3: ORANGE, 4: GREEN, 5: PURPLE, 6: YELLO}
+
+BLOCK_SIZE = 20
+LINE_SIZE = 5
+START_POINT_X = 300
+START_POINT_Y = 200
+
+size = [1000, 800]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Game Title")
+
+
+done = False
+clock = pygame.time.Clock()
+
+
+def draw_block(screen, x, y, color):
+    pygame.draw.rect(screen, color, [int(START_POINT_X + x * BLOCK_SIZE), int(START_POINT_Y + y * BLOCK_SIZE),
+                     BLOCK_SIZE, BLOCK_SIZE], 0)
+    pygame.draw.rect(screen, BLACK, [int(START_POINT_X + x * BLOCK_SIZE), int(START_POINT_Y + y * BLOCK_SIZE),
+                     BLOCK_SIZE, BLOCK_SIZE], 1)
+
+
+array = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, ],
+         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, ],
+         [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, ]]
+
+
+class Move(Enum):
+    TURN = 0
+    LEFT = 1
+    RIGHT = 2
+    DROP = 3
+    DOWN = 4
+
+
+class Front:
+    def show(self, map):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        screen.fill(WHITE)
+        # pygame.draw.polygon(screen, GREEN, [[30,150], [125,100], [220,150]],5)
+        # pygame.draw.polygon(screen, GREEN, [[30,150], [125,100], [220,150]],0)
+        # pygame.draw.lines(screen, RED,False, [[50,150], [50,250], [200,250], [200,150]],5)
+        pygame.draw.rect(screen, BLACK, [int(START_POINT_X - 2), int(START_POINT_Y - 2),
+                                         int(BLOCK_SIZE * 10 + LINE_SIZE / 2), int(BLOCK_SIZE * 25 + LINE_SIZE / 2)], int(LINE_SIZE / 2))
+        # pygame.draw.rect(screen, BLUE, [75,175,75,50],0)
+        # pygame.draw.line(screen, BLACK, [112,175], [112,225],5)
+        # pygame.draw.line(screen, BLACK, [75,200], [150,200],5)
+
+        for y, row in enumerate(map):
+            for x, block in enumerate(row):
+                if block in colors:
+                    draw_block(screen, x, y, colors[block])
+
+        pygame.display.update()
+
+    def get_command(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]:
+            return Move.LEFT
+        if pressed[pygame.K_RIGHT]:
+            return Move.RIGHT
+        if pressed[pygame.K_UP]:
+            return Move.TURN
+        if pressed[pygame.K_DOWN]:
+            return Move.DOWN
+        if pressed[pygame.K_SPACE]:
+            return Move.DROP
+        return -1
+
+    def quit(self):
+        pygame.quit()
